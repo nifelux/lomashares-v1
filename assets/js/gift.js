34f453d24ef2c72212966a191cfe3c2da1f955c8
nifelux <globalpaysure@gmail.com>
2026-03-07 03:05:40 +0100

    Update gift.js

--- assets/js/gift.js
+++ assets/js/gift.js
@@ -1,1 +1,50 @@
+(function () {
 
+"use strict";
+
+async function redeem() {
+
+const code = document.getElementById("code").value;
+
+if (!code) {
+alert("Enter gift code");
+return;
+}
+
+const session = await LomaAuth.requireAuth();
+
+const res = await fetch("/api/gift", {
+
+method: "POST",
+
+headers: {
+"Content-Type": "application/json"
+},
+
+body: JSON.stringify({
+
+user_id: session.user.id,
+code: code
+
+})
+
+});
+
+const data = await res.json();
+
+if (!data.ok) {
+
+alert(data.error);
+return;
+
+}
+
+alert("₦" + data.amount + " credited");
+
+location.reload();
+
+}
+
+window.redeem = redeem;
+
+})();
