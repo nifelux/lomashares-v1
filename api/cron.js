358401ed8466581a8e8a773c49c536b9a644d163
nifelux <globalpaysure@gmail.com>
2026-03-07 02:37:26 +0100

    Update cron.js

--- api/cron.js
+++ api/cron.js
@@ -1,1 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
 
+export default async function handler(req, res) {
+
+const supabase = createClient(
+process.env.SUPABASE_URL,
+process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+const { data, error } = await supabase.rpc("credit_daily_profit");
+
+if(error){
+return res.status(500).json(error);
+}
+
+res.status(200).json(data);
+
+}
